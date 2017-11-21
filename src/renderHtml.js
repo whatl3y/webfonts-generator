99573899d6f261d2130e8d17ab1700ad47b67408
Lance <whatl3y@gmail.com>
2017-11-21 12:04:35 -0500

    Use CSS for styles

--- src/renderHtml.js
+++ src/renderHtml.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var path = require('path')
 var _ = require('underscore')
 var handlebars = require('handlebars')
-var sass = require('node-sass')
+// var sass = require('node-sass')
 
 var renderCss = require('./renderCss')
 
@@ -19,13 +19,13 @@ var renderHtml = function(options) {
 	// have path to fonts that is relative to html file location.
 	// TODO determine if CSS provided in options is SCSS or CSS, and update
 	// accordingly
-	// var styles = renderCss(_.extend({}, options, {
-	// 	cssFontPath: htmlFontsPath
-	// }))
-	var sassString = renderCss(_.extend({}, options, {
+	var styles = renderCss(_.extend({}, options, {
 		cssFontPath: htmlFontsPath
 	}))
-	var styles = sass.renderSync({ data: sassString }).css
+	// var sassString = renderCss(_.extend({}, options, {
+	// 	cssFontPath: htmlFontsPath
+	// }))
+	// var styles = sass.renderSync({ data: sassString }).css
 
 	var ctx = _.extend({
 		names: options.names,
